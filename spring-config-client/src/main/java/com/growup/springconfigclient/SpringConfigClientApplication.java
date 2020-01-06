00aec966f5ac6429dbef6746b0af8ba6bc56807e
Salim Hamidi <salim.hamidi@gmail.com>
2020-01-06 17:07:01 +0100

    First commit

---
+++ spring-config-client/src/main/java/com/growup/springconfigclient/SpringConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.growup.springconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigClientApplication.class, args);
+	}
+
+}
